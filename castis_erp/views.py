69bfc05220b62512b342ed819d8a8692e2c8bf7f
YangHeesung <fshilver@gmail.com>
2018-10-22 22:40:50 +0900

    login redirect url 변경(사용중인 장비목록)

--- castis_erp/views.py
+++ castis_erp/views.py
@@ -1,9 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
-class HomeView(TemplateView):
-    template_name = 'home.html'
+class HomeView(RedirectView):
+    url = reverse_lazy('ems:used_eq_list')
     
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
